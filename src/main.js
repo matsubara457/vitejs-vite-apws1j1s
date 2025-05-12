055d8453634bba3334dc0792ac0a746addc2965b
matsubara457 <tatsuhiro.matsubara@nahato.co.jp>
2025-05-13 08:52:15 +0900

    分割代入の記載方法について

--- src/main.js
+++ src/main.js
@@ -84,8 +84,33 @@
 // }
 // console.log(func2('func2です'))
 
-const func3 = (num1,num2) => ({
-    hoge : num1 ,
-    huga : num2,
-});
-console.log(func3(10,14))
+// const func3 = (num1,num2) => ({
+//     hoge : num1 ,
+//     huga : num2,
+// });
+// console.log(func3(10,14))
+
+// /**
+//  * 分割代入
+//  */
+// const myProfile = {
+//     name : '松原',
+//     age : 26
+// }
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`
+// console.log(message1);
+
+// const { name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ['松原', 26];
+
+// const message3 =  `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`
+// console.log(message3);
+
+// const [ name, age ] = myProfile;
+// const message4 =  `名前は${name}です。年齢は${age}歳です。`
+// console.log(message4);
+
