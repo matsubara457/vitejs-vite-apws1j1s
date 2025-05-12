27f5ad8d4dafde8ced7f2804aaa7ea49f5823375
matsubara457 <tatsuhiro.matsubara@nahato.co.jp>
2025-05-12 23:46:47 +0900

    4-1 変数宣言

--- src/main.js
+++ src/main.js
@@ -1,1 +1,51 @@
-console.log('hoge')
+// /**
+//  * const ,let 党の変数宣言
+//  */
+
+// var val1 = 'var変数';
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = 'var変数を上書き可能';
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = 'var変数を再宣言';
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// // letは上書き可能
+// val2 = 'let変数を上書き';
+
+// // letは再宣言不可能
+// let val2 = 'letを際宣言';
+
+// const val3 = 'const変数';
+// console.log(val3);
+
+// // constは上書き不可
+// val3 = 'const変数を上書き';
+// console.log(val3);
+
+// // constは再宣言不可能
+// let const3 = 'letを際宣言';
+// console.log(val3);
+
+// constで定義したオブジェクトはプロパティの変更が可能
+const val4 = {
+    name: '松原',
+    age: 25,
+  };
+  console.log(val4);
+  val4.name = 'matsubara';
+  val4.address = 'tokyo';
+  console.log(val4);
+  
+  // constで定義した配列はプロパティの変更が可能
+  const val5 = ['dog', 'cat'];
+  val5[0] = 'bird';
+  val5.push('monkey');
+  console.log(val5);
+  
