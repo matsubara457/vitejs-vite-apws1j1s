4a36b792c593bdd1eb9f6f6e181bfa4765342918
matsubara457 <tatsuhiro.matsubara@nahato.co.jp>
2025-05-14 09:12:18 +0900

    三項演算子

--- src/main.js
+++ src/main.js
@@ -200,3 +200,21 @@
 //     }
 // })
 // console.log(newNameArr)
+
+// /**
+//  * 三項演算子
+//  */
+// // ある条件　？　条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof name === 'number' ? num.toLocaleString() : "数値を入力してください"
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) =>{
+//     return num1 + num2 > 100 ? console.log("100を超えています") : "許容範囲内です";
+// }
+// console.log(checkSum(10,100))
